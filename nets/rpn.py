2cd1d571b8714517df0afb8667a81ccf435cc121
Ulricman <83849170+Ulricman@users.noreply.github.com>
2022-04-21 14:03:44 +0800

    Update rpn.py

--- nets/rpn.py
+++ nets/rpn.py
@@ -7,25 +7,23 @@ from utils.anchors import _enumerate_shifted_anchor, generate_anchor_base
 from utils.utils_bbox import loc2bbox
 
 
+def normal_init(m, mean, stddev, truncated=False):
+	if truncated:
+		m.weight.data.normal_().fmod_(2).mul_(stddev).add_(mean)  # not a perfect approximation
+	else:
+		m.weight.data.normal_(mean, stddev)
+		m.bias.data.zero_()
+
+
 class ProposalCreator:
 	def __init__(self, mode, nms_iou=0.7, n_train_pre_nms=12000, n_train_post_nms=600, n_test_pre_nms=3000,
 				 n_test_post_nms=300, min_size=16):
-		# -----------------------------------#
-		#   设置预测还是训练
-		# -----------------------------------#
-		self.mode = mode
-		# -----------------------------------#
-		#   建议框非极大抑制的iou大小
-		# -----------------------------------#
+		self.mode = mode  # set to train/prediction
 		self.nms_iou = nms_iou
-		# -----------------------------------#
 		#   训练用到的建议框数量
-		# -----------------------------------#
 		self.n_train_pre_nms = n_train_pre_nms
 		self.n_train_post_nms = n_train_post_nms
-		# -----------------------------------#
 		#   预测用到的建议框数量
-		# -----------------------------------#
 		self.n_test_pre_nms = n_test_pre_nms
 		self.n_test_post_nms = n_test_post_nms
 		self.min_size = min_size
@@ -38,73 +36,46 @@ class ProposalCreator:
 			n_pre_nms = self.n_test_pre_nms
 			n_post_nms = self.n_test_post_nms
 
-		# -----------------------------------#
-		#   将先验框转换成tensor
-		# -----------------------------------#
-		anchor = torch.from_numpy(anchor)
+		anchor = torch.from_numpy(anchor)  # turn anchor into tensors.
 		if loc.is_cuda:
 			anchor = anchor.cuda()
-		# -----------------------------------#
-		#   将RPN网络预测结果转化成建议框
-		# -----------------------------------#
+		# turn the predicted results by RPN into roi
 		roi = loc2bbox(anchor, loc)
-		# -----------------------------------#
-		#   防止建议框超出图像边缘
-		# -----------------------------------#
+
+		#  prevent the roi from going beyond the boundary.
 		roi[:, [0, 2]] = torch.clamp(roi[:, [0, 2]], min=0, max=img_size[1])
 		roi[:, [1, 3]] = torch.clamp(roi[:, [1, 3]], min=0, max=img_size[0])
 
-		# -----------------------------------#
-		#   建议框的宽高的最小值不可以小于16
-		# -----------------------------------#
+		# the height and width should not less than 16
 		min_size = self.min_size * scale
 		keep = torch.where(((roi[:, 2] - roi[:, 0]) >= min_size) & ((roi[:, 3] - roi[:, 1]) >= min_size))[0]
-		# -----------------------------------#
-		#   将对应的建议框保留下来
-		# -----------------------------------#
 		roi = roi[keep, :]
 		score = score[keep]
 
-		# -----------------------------------#
-		#   根据得分进行排序，取出建议框
-		# -----------------------------------#
 		order = torch.argsort(score, descending=True)
 		if n_pre_nms > 0:
 			order = order[:n_pre_nms]
 		roi = roi[order, :]
 		score = score[order]
 
-		# -----------------------------------#
-		#   对建议框进行非极大抑制
-		#   使用官方的非极大抑制会快非常多
-		# -----------------------------------#
 		keep = nms(roi, score, self.nms_iou)
 		keep = keep[:n_post_nms]
 		roi = roi[keep]
 		return roi
 
 
-class RegionProposalNetwork(nn.Module):
+class RegionProposalNetwork(nn.Module):  # RPN
 	def __init__(self, in_channels=512, mid_channels=512, ratios=(0.5, 1, 2), anchor_scales=(8, 16, 32), feat_stride=16,
 				 mode="training"):
 		super(RegionProposalNetwork, self).__init__()
-		# -----------------------------------------#
-		#   生成基础先验框，shape为[9, 4]
-		# -----------------------------------------#
+		# generate base anchors (shape is (9, 4))
 		self.anchor_base = generate_anchor_base(anchor_scales=anchor_scales, ratios=ratios)
 		n_anchor = self.anchor_base.shape[0]
 
-		# -----------------------------------------#
-		#   先进行一个3x3的卷积，可理解为特征整合
-		# -----------------------------------------#
 		self.conv1 = nn.Conv2d(in_channels, mid_channels, (3, 3), (1, 1), 1)
-		# -----------------------------------------#
-		#   分类预测先验框内部是否包含物体
-		# -----------------------------------------#
+		# the size of the feature map stays the same.
+
 		self.score = nn.Conv2d(mid_channels, n_anchor * 2, (1, 1), (1, 1), 0)
-		# -----------------------------------------#
-		#   回归预测对先验框进行调整
-		# -----------------------------------------#
 		self.loc = nn.Conv2d(mid_channels, n_anchor * 4, (1, 1), (1, 1), 0)
 
 		# -----------------------------------------#
@@ -114,30 +85,22 @@ class RegionProposalNetwork(nn.Module):
 		# -----------------------------------------#
 		#   用于对建议框解码并进行非极大抑制
 		# -----------------------------------------#
-		self.proposal_layer = ProposalCreator(mode)
-		# --------------------------------------#
-		#   对FPN的网络部分进行权值初始化
-		# --------------------------------------#
+		self.proposal_layer = ProposalCreator(mode=mode)
+
+		# weight initialization for RPN.
 		normal_init(self.conv1, 0, 0.01)
 		normal_init(self.score, 0, 0.01)
 		normal_init(self.loc, 0, 0.01)
 
 	def forward(self, x, img_size, scale=1.):
-		n, _, h, w = x.shape
-		# -----------------------------------------#
-		#   先进行一个3x3的卷积，可理解为特征整合
-		# -----------------------------------------#
-		x = F.relu(self.conv1(x))
-		# -----------------------------------------#
-		#   回归预测对先验框进行调整
-		# -----------------------------------------#
-		rpn_locs = self.loc(x)
-		rpn_locs = rpn_locs.permute(0, 2, 3, 1).contiguous().view(n, -1, 4)
-		# -----------------------------------------#
-		#   分类预测先验框内部是否包含物体
-		# -----------------------------------------#
-		rpn_scores = self.score(x)
-		rpn_scores = rpn_scores.permute(0, 2, 3, 1).contiguous().view(n, -1, 2)
+		n, _, h, w = x.shape  # (4, 1024, 38, 38)
+		x = F.relu(self.conv1(x))  # (4, 512, 38, 38)
+
+		rpn_locs = self.loc(x)  # (4, 36, 38, 38)
+		rpn_locs = rpn_locs.permute(0, 2, 3, 1).contiguous().view(n, -1, 4)  # (4, 12996, 4)
+
+		rpn_scores = self.score(x)  # (4, 18, 38, 38)
+		rpn_scores = rpn_scores.permute(0, 2, 3, 1).contiguous().view(n, -1, 2)  # (4, 12996, 2)
 
 		# --------------------------------------------------------------------------------------#
 		#   进行softmax概率计算，每个先验框只有两个判别结果
@@ -164,11 +127,3 @@ class RegionProposalNetwork(nn.Module):
 		roi_indices = torch.cat(roi_indices, dim=0)
 
 		return rpn_locs, rpn_scores, rois, roi_indices, anchor
-
-
-def normal_init(m, mean, stddev, truncated=False):
-	if truncated:
-		m.weight.data.normal_().fmod_(2).mul_(stddev).add_(mean)  # not a perfect approximation
-	else:
-		m.weight.data.normal_(mean, stddev)
-		m.bias.data.zero_()
